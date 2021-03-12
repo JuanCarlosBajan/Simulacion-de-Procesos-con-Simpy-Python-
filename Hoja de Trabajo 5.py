63565894f80600908d47ab8832b324ac3e5de34c
JuanCarlosBajan <baj20109@uvg.edu.gt>
2021-03-11 18:24:08 -0600

    Continuacion de codigo

--- Hoja de Trabajo 5.py
+++ Hoja de Trabajo 5.py
@@ -8,3 +8,14 @@ def Process(env,name, Ram,cpu, memoria, instrucciones, redIns, tiempoEspera, Tie
     cc =0 
     run = True
     tiempoInterno = tiempoEspera
+    yield env.timeout(tiempoEspera)
+    print("Iniciando proceso %s, entrando a lista ready. %s" % (name, env.now))
+    pivoteTiempo = env.now
+    while run == True:
+        yield Ram.get(memoria)
+        tiempoInterno = tiempoInterno + env.now-pivoteTiempo +1
+        while instrucciones > 0:
+            pivoteTiempo = env.now
+            with cpu.request() as req:
+                yield req
+                tiempoInterno = tiempoInterno + env.now-pivoteTiempo +1
